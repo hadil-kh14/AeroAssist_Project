f196114dc0dd80ed58fe72480ce6a0da8d97069c
hadil khaireddine <khaireddinehadil07@gmail.com>
2026-06-13 10:27:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,26 @@ from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Charger la clé API depuis le fichier .env
+
 load_dotenv()
 groq_key = os.getenv("GROQ_API_KEY")
 
 st.title("AeroAssist ✈️")
 st.subheader("Assistant IA de Maintenance avec Grok")
 
-# Connexion à l'API Grok (via l'interface compatible OpenAI)
+
 llm = ChatOpenAI(
     openai_api_key=groq_key,
-    openai_api_base="https://api.groq.com/openai/v1",  # Serveur Groq
-    model_name="llama-3.3-70b-versatile"              # Modèle gratuit ultra-performant
+    openai_api_base="https://api.groq.com/openai/v1",  
+    model_name="llama-3.3-70b-versatile"              
 )
 
-# Simulation du bouton de recherche
+
 query = st.text_input("Exemple : Comment réparer une fuite de compresseur ?")
 
 if query:
     st.write("🔄 Connexion à Llama-3 en cours...")
-    # Ici LangChain enverra la question à Grok
+    
     try:
         reponse = llm.invoke(query)
         st.success("Réponse de l'assistant :")
